a5aa7c6e81013e7d0b9aa7acd39113b63224de12
HinakoUmori <takumasano98@yahoo.co.jp>
2021-01-18 21:20:11 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace BetManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.colNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colOdds = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumberOfTrails = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvList
+            // 
+            this.dgvList.AllowUserToAddRows = false;
+            this.dgvList.AllowUserToDeleteRows = false;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colNo,
+            this.colOdds});
+            this.dgvList.Location = new System.Drawing.Point(11, 46);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.RowHeadersWidth = 56;
+            this.dgvList.RowTemplate.Height = 25;
+            this.dgvList.Size = new System.Drawing.Size(1029, 390);
+            this.dgvList.TabIndex = 0;
+            // 
+            // colNo
+            // 
+            this.colNo.HeaderText = "No";
+            this.colNo.MinimumWidth = 7;
+            this.colNo.Name = "colNo";
+            this.colNo.Width = 135;
+            // 
+            // colOdds
+            // 
+            this.colOdds.HeaderText = "Odds";
+            this.colOdds.MinimumWidth = 7;
+            this.colOdds.Name = "colOdds";
+            this.colOdds.Width = 135;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(122, 28);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(131, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(122, 28);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExecute.Location = new System.Drawing.Point(259, 3);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(124, 28);
+            this.btnExecute.TabIndex = 3;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnExecute, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnDelete, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 442);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(386, 35);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of trails";
+            // 
+            // txtNumberOfTrails
+            // 
+            this.txtNumberOfTrails.Location = new System.Drawing.Point(302, 17);
+            this.txtNumberOfTrails.Name = "txtNumberOfTrails";
+            this.txtNumberOfTrails.Size = new System.Drawing.Size(96, 29);
+            this.txtNumberOfTrails.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 512);
+            this.Controls.Add(this.txtNumberOfTrails);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dgvList);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 12.18462F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colOdds;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumberOfTrails;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetManager
+{
+    public partial class Form1 : Form
+    {
+        private enum DGV_LIST {
+            No,
+            Odds
+        }
+        List<int> min_profit = new List<int>();
+        List<int> max_profit = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            dgvList.Rows.Add("", "");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            dgvList.Rows.Remove(dgvList.CurrentRow);
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            float castable;
+            int trails = 0;
+            int sum_bet = 0;
+            Boolean flag = false;
+            int[] bet;
+            float[] odds;
+            float[] refund; ;
+            DataGridViewTextBoxColumn textColumn;
+            try
+            {
+                while (dgvList.Columns.Count > 2)
+                {
+                    dgvList.Columns.RemoveAt(2);
+                }
+                for (int i=0; i<dgvList.Rows.Count; i++)
+                {
+                    if (dgvList.Rows[i].Cells[0].Value.ToString().Equals("min"))
+                    {
+                        dgvList.Rows.RemoveAt(i);
+                    }
+                    if (dgvList.Rows[i].Cells[0].Value.ToString().Equals("max"))
+                    {
+                        dgvList.Rows.RemoveAt(i);
+                    }
+                }
+                odds = new float[dgvList.Rows.Count];
+                refund = new float[dgvList.Rows.Count];
+                for (int i=0; i < dgvList.Rows.Count; i++)
+                {
+                    if (dgvList.Rows[i].Cells[(int)DGV_LIST.No].Value.ToString().Trim() == "")
+                    {
+                        throw new Exception("There is a blank field in No ");
+                    }
+                    if (dgvList.Rows[i].Cells[(int)DGV_LIST.Odds].Value.ToString().Trim() == "")
+                    {
+                        throw new Exception("There is a blank field in Odds");
+                    }
+                    if (float.TryParse(dgvList.Rows[i].Cells[(int)DGV_LIST.Odds].Value.ToString(), out castable))
+                    {
+                    }
+                    else
+                    {
+                        throw new Exception("There is a string field in Odds");
+                    }
+                }
+                dgvList.Sort(dgvList.Columns[(int)DGV_LIST.Odds], System.ComponentModel.ListSortDirection.Ascending);
+                trails = int.Parse(txtNumberOfTrails.Text.Trim());
+                bet = new int[dgvList.Rows.Count];
+                for (int i=0; i<dgvList.Rows.Count; i++)
+                {
+                    odds[i] = float.Parse(dgvList.Rows[i].Cells[1].Value.ToString());
+                    bet[i] = 1;
+                }
+
+                for (int i=0; i<trails; i++)
+                {
+                    textColumn = new DataGridViewTextBoxColumn();
+                    textColumn.Name = "col" + i;
+                    textColumn.HeaderText = (i+1).ToString();
+                    dgvList.Columns.Add(textColumn);
+                    sum_bet = 0;
+                    
+                    for(int j=0; j<dgvList.Rows.Count; j++)
+                    {
+                        refund[j] = 0;
+                    }
+
+                    for(int j=0; j<dgvList.Rows.Count; j++)
+                    {
+                        dgvList.Rows[j].Cells[i + 2].Value = bet[j];
+                        refund[j] = bet[j] * odds[j] * 100;
+                        sum_bet += bet[j] * 100;
+                    }
+
+                    min_profit.Add((int)(refund.Min() - sum_bet + 0.5));
+                    max_profit.Add((int)(refund.Max() - sum_bet + 0.5));
+
+                    flag = true;
+                    for(int j=0; j<dgvList.Rows.Count; j++)
+                    {
+                        if(refund[j] < sum_bet)
+                        {
+                            bet[j] += 1;
+                            flag = false;
+                        }
+                    }
+                    if (flag == true)
+                    {
+                        for (int k = 0; k < dgvList.Rows.Count; k++)
+                        {
+                            bet[k] += 1;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < dgvList.Rows.Count; i++)
+                {
+                    bet[i] = int.Parse(dgvList.Rows[i].Cells[dgvList.Columns.Count - 1].Value.ToString());
+                    refund[i] = bet[i] * odds[i] * 100;
+                    sum_bet += bet[i] * 100;
+                }
+                min_profit.Add((int)(refund.Min() - sum_bet + 0.5));
+                max_profit.Add((int)(refund.Max() - sum_bet + 0.5));
+
+                dgvList.Rows.Add();
+                for (int i=2; i<dgvList.Columns.Count ; i++)
+                {
+                    dgvList.Rows[dgvList.Rows.Count - 1].Cells[i].Value = min_profit[i - 2];
+                }
+                dgvList.Rows.Add();
+                for (int i=2; i < dgvList.Columns.Count; i++)
+                {
+                    dgvList.Rows[dgvList.Rows.Count - 1].Cells[i].Value = max_profit[i - 2];
+                }
+
+                dgvList.Rows[dgvList.Rows.Count - 2].Cells[0].Value = "min";
+                dgvList.Rows[dgvList.Rows.Count - 2].Cells[1].Value = "";
+                dgvList.Rows[dgvList.Rows.Count - 1].Cells[0].Value = "max";
+                dgvList.Rows[dgvList.Rows.Count - 1].Cells[1].Value = "";
+
+                for(int i=2; i < dgvList.Columns.Count; i++)
+                {
+                    if(int.Parse(dgvList.Rows[dgvList.Rows.Count - 2].Cells[i].Value.ToString()) >= 0)
+                    {
+                        dgvList.Columns[i].DefaultCellStyle.BackColor = Color.LightBlue;
+                    }
+                }
+                max_profit.Clear();
+                min_profit.Clear();
+            } 
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //testData
+            dgvList.Rows.Add(10, 5.3);
+            dgvList.Rows.Add(15, 2.1);
+            dgvList.Rows.Add(13, 11.8);
+            dgvList.Rows.Add(4, 12.1);
+
+        }
+    }
+}
